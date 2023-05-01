58b4db1b8e56af6e453bd1c18b92132c3642f2ae
Joaquin Paradela <joaquin.paradela28@gmail.com>
2023-05-01 17:08:32 -0300

    vista agregar 0.1

---
+++ resources/views/admin/beats.blade.php
@@ -0,0 +1,75 @@
+<x-admin-layout>
+	<!-- Bootstrap 
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-datepicker.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">-->
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
+ 
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+<body>
+	
+	<div class="container">
+		<div class="content">
+			<h2>Productos &raquo; Agregar</h2>
+			<hr />
+
+			<form class="form-horizontal" action="" method="post">
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Nombre</label>
+					<div class="col-sm-2">
+						<input type="text" name="name" class="form-control" placeholder="Nombre" required>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Descripcion</label>
+					<div class="col-sm-3">
+						<textarea name="descripcion" class="form-control" placeholder="Descripcion"></textarea>
+					</div>
+				</div>
+
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Imagen</label>
+                    <div class="col-sm-3">
+                    <input type="file" name="imagen" class="form-control-file">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col-sm-3 control-label">Audio</label>
+                    <div class="col-sm-3">
+                    <input type="file" name="audio" class="form-control-file">
+                    </div>
+                </div>
+  
+				<div class="form-group">
+					<label class="col-sm-3 control-label">Precio</label>
+					<div class="col-sm-3">
+						<input type="text" name="price" class="form-control" placeholder="Precio" required>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label class="col-sm-3 control-label">&nbsp;</label>
+					<div class="col-sm-6">
+						<input type="submit" name="add" class="btn btn-sm btn-primary" value="Guardar datos">
+						<a href="index.php" class="btn btn-sm btn-danger">Cancelar</a>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+ 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+
+</x-admin-layout>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -42,11 +42,7 @@
        @else
        
       
-       <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-
-        
-            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>   
-        
+       <h1>SOLO ADMINISTRADOR</h1>
         
           @endif
             <!-- Page Content -->
@@ -57,6 +53,7 @@
             </main>
 
         </div>
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
